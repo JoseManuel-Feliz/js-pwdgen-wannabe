a3428554679d3db527d5a17b8f70132f0a7ecb3c
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-07-30 16:13:05 +0200

    Creazione step da eseguire

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log('js ok')
+
+// Raccolta dati dell'utente per generazione password
+
+/* 
+1. Recuperare l'elemento con id:password all'interno del DOM.
+
+2. Chiedere il nome all'utente.
+    2a. Verificare che sia stato raccolto ed inserito correttamente in console.
+
+3. Chiedere il cognome all'utente.
+    3a. Verificare che sia stato raccolto ed inserito correttamente in console.
+
+4. Chiedere il colore preferito all'utente.
+    4a. Verificare che sia stato raccolto ed inserito correttamente in console.
+
+5. Inserire i dati raccolti all'interno dell'elemento con id:password.
+*/
