fdac947f610427fa71904cf951463e5e763fe1a5
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-07-30 17:21:23 +0200

    add fix and update steps

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ console.log('js ok')
 
 /* 
 1. Recuperare l'elemento con id:password all'interno del DOM.
-    1b. Verificare inserito correttamente in console.
+    1b. Verificare che sia stato inserito correttamente in console.
 
 2. Chiedere il nome all'utente.
     2a. Verificare che sia stato raccolto ed inserito correttamente in console.
@@ -15,14 +15,20 @@ console.log('js ok')
 4. Chiedere il colore preferito all'utente.
     4a. Verificare che sia stato raccolto ed inserito correttamente in console.
 
-5. Inserire i dati raccolti all'interno dell'elemento con id:password.
+5. Inserire elemento per fortificare la password
+    5b. Verificare che sia stato inserito correttamente in console.
+
+
+6. Inserire i dati raccolti all'interno dell'elemento con id:password.
+6b. Verificare inserito correttamente in console e nel DOM.
+
 */
 
 /* Fase Preparatoria */
 
 //  1. Recuperare l'elemento con id:password all'interno del DOM.
 const passwordElement = document.getElementById('password')
-//1b. Verificare inserito correttamente in console.
+//1b. Verificare che sia stato inserito correttamente in console.
 console.log(password)
 
 /* Raccolta dati */
@@ -30,14 +36,20 @@ console.log(password)
 // 2. Chiedere il nome all'utente.
 const nome = prompt('Qual è il tuo nome?', 'Jose');
 //2a. Verificare che sia stato raccolto ed inserito correttamente in console.
-console.log(nome)
+console.log(nome, typeof nome)
 
 // 3. Chiedere il cognome all'utente.
 const cognome = prompt('Qual è il tuo cognome?', 'Feliz');
 //3a. Verificare che sia stato raccolto ed inserito correttamente in console.
-console.log(cognome)
+console.log(cognome, typeof cognome)
 
 // 4. Chiedere il colore preferito all'utente.
 const colore = prompt('Qual è il tuo colore preferito?', 'Verde');
 //4a. Verificare che sia stato raccolto ed inserito correttamente in console.
-console.log(nome)
+console.log(colore, typeof colore)
+
+// 5. Inserimento di elemento per fortificare la password
+const strongpass = 21
+//5b. Verificare inserito correttamente in console.
+console.log(strongpass, typeof strongpass)
+
