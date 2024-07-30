d83151458ed7c8777cf8360d8b5630de512125ca
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-07-30 16:40:04 +0200

    add fase raccolta dati

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ console.log('js ok')
 
 /* 
 1. Recuperare l'elemento con id:password all'interno del DOM.
+    1b. Verificare inserito correttamente in console.
 
 2. Chiedere il nome all'utente.
     2a. Verificare che sia stato raccolto ed inserito correttamente in console.
@@ -21,3 +22,22 @@ console.log('js ok')
 
 //  1. Recuperare l'elemento con id:password all'interno del DOM.
 const passwordElement = document.getElementById('password')
+//1b. Verificare inserito correttamente in console.
+console.log(password)
+
+/* Raccolta dati */
+
+// 2. Chiedere il nome all'utente.
+const nome = prompt('Qual è il tuo nome?', 'Jose');
+//2a. Verificare che sia stato raccolto ed inserito correttamente in console.
+console.log(nome)
+
+// 3. Chiedere il cognome all'utente.
+const cognome = prompt('Qual è il tuo cognome?', 'Feliz');
+//3a. Verificare che sia stato raccolto ed inserito correttamente in console.
+console.log(cognome)
+
+// 4. Chiedere il colore preferito all'utente.
+const colore = prompt('Qual è il tuo colore preferito?', 'Verde');
+//4a. Verificare che sia stato raccolto ed inserito correttamente in console.
+console.log(nome)
